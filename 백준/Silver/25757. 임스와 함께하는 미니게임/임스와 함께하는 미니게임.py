bdde118add436266e4c21d6a14f8fce30a338b43
Junho Jang <104313883+imjjh@users.noreply.github.com>
2025-12-16 08:59:06 +0900

    [Silver V] Title: 임스와 함께하는 미니게임, Time: 120 ms, Memory: 118736 KB -BaekjoonHub

--- 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
+++ 백준/Silver/25757. 임스와 함께하는 미니게임/임스와 함께하는 미니게임.py
@@ -1,4 +1,7 @@
-n,selected_game= input().split()
+import sys
+
+
+n,selected_game= sys.stdin.readline().split()
 
 
 games={'Y':2,'F':3,'O':4}
@@ -6,7 +9,7 @@ games={'Y':2,'F':3,'O':4}
 names=set()
 
 for _ in range(int(n)):
-    names.add(input())
+    names.add(sys.stdin.readline())
 
 
 print(len(names)//(games[selected_game]-1))
